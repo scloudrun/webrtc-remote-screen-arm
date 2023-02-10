7ba211a4357961fef4f526c2cd526d59217c8809
scloudrun <scloudrun@github.com>
2023-02-10 09:05:11 +0000

    Feat suuport minicap -Q

--- internal/rdisplay/shell.go
+++ internal/rdisplay/shell.go
@@ -94,8 +94,17 @@ func RemoveFile(path string) error {
 }
 
 
+/**
+minicap 内部流程解析
+(1) 解析 LD_LIBRARY_PATH=/data/local/tmp /data/local/tmp/minicap -P 1440x2560@1440x2560/0 启动行参数，设置相关配置。
+
+  其中：
+   -P：(x@x/{0|90|180|270}) minicap 产生的图片尺寸以及传递出来的图片尺寸,可修改该参数调整 client 收到的图片的宽度或高度。eg：1080x1920@360x640/0 指定 minicap 在手机内部产生图片分辨率为 1080x1920，而传递出来的图片分辨率为 360x640,0 表示手机目前是竖屏状态，逆时针旋转手机可根据不同角度设置此参数为 90、180、270。
+   -Q： 设置 minicap 内部对图片的压缩比：0—100
+  注：minicap 内部会对期望图片的分辨率做一个调整，如果期望的分辨率大小和手机的真实分辨率大小是等比的就不做调整，如果不等比则会调整期望的图片的分辨率大小。
+**/
 func run() {
-	shellCmd := "LD_LIBRARY_PATH=/data/local/tmp /data/local/tmp/minicap -P \"1080x1920@1080x1920/0\" -s > /data/local/tmp/h264mini/"+ToString(time.Now().UnixNano()/int64(time.Millisecond))+".jpg"
+	shellCmd := "LD_LIBRARY_PATH=/data/local/tmp /data/local/tmp/minicap -Q 70 -P \"1080x1920@540x960/0\" -s > /data/local/tmp/h264mini/"+ToString(time.Now().UnixNano()/int64(time.Millisecond))+".jpg"
 	_,err := RunCommand(shellCmd)
 	if err !=nil {
 		fmt.Println(time.Now().Unix())
